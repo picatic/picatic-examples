e6342cde47f47a0fd780bb2d3e9173ec28b66e85
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-12 22:48:01 -0700

    Fixed the stripe key integration to be able to use the event owners public stripe key

--- react-checkout/src/index.js
+++ react-checkout/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { StripeProvider } from 'react-stripe-elements'
 import App from './App'
 
 ReactDOM.render(
-  <StripeProvider apiKey="pk_test_tpasl2IDtf7ZGvpRs8iTdwpQ">
-    <Router>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route path="/:id" component={App} />
-      </div>
-    </Router>
-  </StripeProvider>,
-  document.getElementById('root'),
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/:id" component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
 )
