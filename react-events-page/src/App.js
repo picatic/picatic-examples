8d16c8b2c644f250bdaa4d7f6df06d12daeef234
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-13 22:59:03 -0700

    Fetched event data from slug

--- react-events-page/src/App.js
+++ react-events-page/src/App.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+const host = 'https://api-staging.picatic.com/v2'
 
 class App extends Component {
+  state = {
+    event: false
+  }
+  componentWillMount() {
+    const { match } = this.props
+
+    const eventSlug = match.params.slug
+
+    this.getEvent(eventSlug)
+  }
+  getEvent = eventSlug => {
+    const url = `${host}/event/${eventSlug}`
+    fetch(url, { method: 'GET' })
+      .then(res => res.json())
+      .then(event => this.setState({ event: event.data }))
+      .catch(err => console.log(err))
+  }
   render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
+    const { event } = this.state
+
+    const noEvent = !event
+    if (noEvent) {
+      return false
+    }
+
+    return <div>{event.id}</div>
   }
 }
 
-export default App;
+export default App
--- react-events-page/src/index.js
+++ react-events-page/src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/:slug" component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+)
