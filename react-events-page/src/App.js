0167439610dbd4245303ea83156a516270be944c
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-14 19:22:19 -0700

    Added header with dynamic image overlay

--- react-events-page/src/App.js
+++ react-events-page/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import './App.css'
 
 const host = 'https://api-staging.picatic.com/v2'
@@ -29,7 +30,64 @@ class App extends Component {
       return false
     }
 
-    return <div>{event.id}</div>
+    const {
+      title,
+      cover_image_uri,
+      start_date,
+      start_time,
+      end_date,
+      end_time,
+      intro_box_opacity,
+      venue_name
+    } = event.attributes
+
+    const imageURI = cover_image_uri.substring(50)
+
+    const styles = {
+      headerBackground: {
+        background: `url(https://picatic.global.ssl.fastly.net/events/${imageURI}?filter=overlay&colors=070826&opacity=${intro_box_opacity *
+          100}) no-repeat center center / cover`
+      }
+    }
+
+    const startDate = moment(start_date).format('MMMM Do, YYYY')
+    const startTime = `- ${start_time}`
+    const endDate = moment(end_date).format('MMMM Do, YYYY')
+    const endTime = `- ${end_time}`
+
+    return (
+      <div className="container-fluid">
+        <header className="header" style={styles.headerBackground}>
+          <section className="row h-100 text-center">
+            <div className="col-12 align-self-center">
+              <h1 className="text-white">{title}</h1>
+            </div>
+          </section>
+
+          <section className="row header-footer">
+            <div className="col-6 align-self-bottom text-left">
+              <h6>
+                {startDate} {start_time && startTime} -
+              </h6>
+              <h6 className="text-muted">
+                {endDate} {end_time && endTime}
+              </h6>
+            </div>
+            <div className="col-6 align-self-bottom text-right">
+              <h6>{venue_name}</h6>
+            </div>
+          </section>
+        </header>
+
+        <section className="row ticket">
+          <div className="col align-self-center">
+            <a href="#" className="btn btn-primary">
+              Get Tickets
+            </a>
+          </div>
+        </section>
+      </div>
+    )
   }
 }
 
