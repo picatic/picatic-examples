9943bc0c8cfcb37391a6fbd6361521cec890e449
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-16 00:37:28 -0700

    Added Button to bottom nav and formatted date and venue

--- react-events-page/src/components/Button/index.js
+++ react-events-page/src/components/Button/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './Button.css'
 
 class Button extends Component {
   render() {
     const { eventId } = this.props
     const eventCheckout = `https://www.picatic.com/${eventId}`
     return (
-      <a href={eventCheckout} className="btn btn-primary">
+      <a href={eventCheckout} className="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect mdl-button-anywhere">
         Get Tickets
       </a>
     )
