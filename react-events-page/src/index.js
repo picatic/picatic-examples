680b905dee0750a308c53acbd5d5f57867be6275
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-26 23:17:42 -0700

    Added link to home page component to render list of events for a particular team

--- react-events-page/src/index.js
+++ react-events-page/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
 import App from './App'
+
 
 ReactDOM.render(
   <Router>
     <div>
+      <Route exact path="/" component={Home} />
       <Route path="/:slug" component={App} />
     </div>
   </Router>,
