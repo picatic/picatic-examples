9f138f9206689de7e430b1137f5cd28550aaf3ca
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-29 19:21:49 -0700

    Changed functionality from paid ticket to free ticket

--- react-checkout/src/components/Tickets/index.js
+++ react-checkout/src/components/Tickets/index.js
@@ -1,64 +1,26 @@
 import React, { Component } from 'react'
 
-class Tickets extends Component {
-  state = {
-    selectedQuantity: ''
-  }
-
-  selectTicket = event => {
-    event.preventDefault()
-    const { ticket, selectTickets } = this.props
-    const value = event.target.value
-    selectTickets(ticket.id, value)
-    this.setState({ selectedQuantity: value })
-  }
-
+export default class Tickets extends Component {
   render() {
-    const { ticket } = this.props
-    const { selectedQuantity } = this.state
+    const { ticket, handleClick } = this.props
+    const { name, quantity, quantity_sold } = this.props.ticket.attributes
 
-    const {
-      name,
-      price,
-      max_quantity,
-      quantity,
-      quantity_sold
-    } = this.props.ticket.attributes
-
-    const roundPrice = parseFloat(price)
-
-    const ticketsRemaining = quantity - quantity_sold
-    const maxQuantity =
-      max_quantity > 0
-        ? Math.min(max_quantity, ticketsRemaining)
-        : ticketsRemaining
+    const soldOut = quantity - quantity_sold
 
     return (
-      <div key={ticket.id} className="mdl-grid align-items-center">
-        <div className="mdl-cell mdl-cell--8-col">{name}</div>
-        <div className="mdl-cell mdl-cell--4-col">
-          <div className="form-row align-items-center float-right">
-            {/* TODO: Replace static currency with dynamic value */}
-            <div className="col-auto">${roundPrice}</div>
-            <div className="col-auto">x</div>
-            <div className="col-auto">
-              <div className="input-group">
-                <input
-                  type="number"
-                  className="form-control text-center"
-                  value={selectedQuantity}
-                  onChange={this.selectTicket}
-                  placeholder="0"
-                  min="0"
-                  max={maxQuantity}
-                />
-              </div>
-            </div>
-          </div>
+      <div key={ticket.id} className="row p-3">
+        <div className="col-8 d-flex align-items-center">
+          {name}
+        </div>
+        <div className="col-4 text-right">
+          <button
+            className="mdl-button mdl-button--raised mdl-button--primary mdl-js-button mdl-js-ripple-effect"
+            onClick={() => handleClick(ticket)}
+          >
+            Register
+          </button>
         </div>
       </div>
     )
   }
 }
-
-export default Tickets
--- react-checkout/src/index.js
+++ react-checkout/src/index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { StripeProvider } from 'react-stripe-elements'
 import App from './App'
 
-ReactDOM.render(
-  <StripeProvider apiKey="pk_test_tpasl2IDtf7ZGvpRs8iTdwpQ">
-    <App />
-  </StripeProvider>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
