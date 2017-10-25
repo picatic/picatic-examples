5bba9d965a69c3a5b0c719deaba315d5e5b33778
Andrew Bolt <andrewebolt@gmail.com>
2017-10-24 21:29:23 -0700

    adding angular, updating layout and design

---
+++ picatic.io/app/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var angular = require('angular');
+var ngModule = angular.module('app', []);
+
+require('./components')(ngModule);
+require('./services')(ngModule);
+require('./styles/normalize.css');
+require('./styles/_less/fonts.less')
+require('./styles/_less/bootstrap.less');
+require('./styles/_less/marketing.less');
+require('./styles/app.css');
