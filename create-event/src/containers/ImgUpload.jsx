97177fecd1fca1a1cb8d82f741ca3fdeb8f491eb
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-10-26 12:09:23 -0700

    Rendered uploaded image and connected v3 filestack api widget

--- create-event/src/containers/ImgUpload.jsx
+++ create-event/src/containers/ImgUpload.jsx
@@ -3,14 +3,20 @@ import ReactFilestack from 'filestack-react'
 import uuidv4 from 'uuid/v4'
 
 export default class ImgUpload extends Component {
+
   onSuccess = result => {
-    console.log('Result', result)
+    const url = result.filesUploaded[0].url
+    this.props.handleEventName('cover_image_uri', url)
   }
+
   onError = error => {
     console.log('Error', error)
   }
+
   render() {
-    const { event, apiKey } = this.props
+    const { image, event, apiKey } = this.props
+
+    const hasImage = image !== ''
 
     const imageName = uuidv4()
     const path = `/event/${event.id}/${imageName}`
@@ -24,15 +30,32 @@ export default class ImgUpload extends Component {
         access: 'public'
       }
     }
+
+    const styles = {
+      image: {
+        background: `url(${image}) no-repeat center`,
+        backgroundSize: 'cover',
+        width: 300,
+        height: 200,
+        boxShadow: '0 4px 30px 0 rgba(0, 0, 0, .24)',
+        borderRadius: 5,
+        margin: 'auto'
+      }
+    }
+    const text = hasImage ? '' : 'Upload Image'
     return (
-      <ReactFilestack
-        buttonText="Upload Image"
-        buttonClass="className"
-        apikey={apiKey}
-        options={options}
-        onSuccess={this.onSuccess}
-        onError={this.onError}
-      />
+      <div style={hasImage ? styles.image : {}}>
+        <ReactFilestack
+          buttonText={text}
+          buttonClass={`mdl-button mdl-js-button mdl-js-ripple-effect ${hasImage
+            ? 'image-button'
+            : ''}`}
+          apikey={apiKey}
+          options={options}
+          onSuccess={this.onSuccess}
+          onError={this.onError}
+        />
+      </div>
     )
   }
 }
