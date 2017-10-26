bef72a6ed0f4b81bd105af09d389e2b71e611171
Andrew Bolt <andrewebolt@gmail.com>
2017-10-26 12:45:13 -0700

    Centering bootstrap columns and changing text in each example

--- picatic.io/app/services/ExamplesService.js
+++ picatic.io/app/services/ExamplesService.js
@@ -4,18 +4,18 @@ module.exports = function(ngModule) {
   ngModule.factory('ExamplesService', [ function() {
     var ExamplesService = {
       examples: [
-        {
-          name: 'events calendar',
-          color: 'heart',
-          description: 'It\'s easy to customize each step in the event registration process and maintain your brand guidelines with our Ticketing API.',
-          demoLink: 'https://calendar.picatic.io/',
-          imgUrl: require('../img/calendar.png'),
-          sourceLink: 'https://github.com/picatic/picatic-examples'
-        },
+        // {
+        //   name: 'events calendar',
+        //   color: 'heart',
+        //   description: 'It\'s easy to customize each step in the event registration process and maintain your brand guidelines with our Ticketing API.',
+        //   demoLink: 'https://calendar.picatic.io/',
+        //   imgUrl: require('../img/calendar.png'),
+        //   sourceLink: 'https://github.com/picatic/picatic-examples'
+        // },
         {
           name: 'create events',
           color: 'cerulean',
-          description: 'It\'s easy to customize each step in the event registration process and maintain your brand guidelines with our Ticketing API.',
+          description: 'Create new events quickly from your website or app.',
           demoLink: 'https://event-creator.picatic.io/',
           imgUrl: require('../img/create-events.png'),
           sourceLink: 'https://github.com/picatic/picatic-examples'
@@ -23,10 +23,10 @@ module.exports = function(ngModule) {
         {
           name: 'checkout',
           color: 'dodger',
-          description: 'It\'s easy to customize each step in the event registration process and maintain your brand guidelines with our Ticketing API.',
+          description: 'Customize the registration and payment process with our checkout endpoints.',
           demoLink: 'https://checkout.picatic.io/',
           imgUrl: require('../img/checkout.png'),
-          sourceLink: 'https://github.com/picatic/picatic-examples'
+          sourceLink: 'https://github.com/picatic/picatic-examples/tree/master/react-checkout'
         }
       ]
     };
