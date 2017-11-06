7628e5720c1cebd7eca45f14f74278e3fcce71f8
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-11-06 14:56:10 -0800

    Updated var to const and let
    Reformatted files

--- picatic-slack-rapid/src/post.js
+++ picatic-slack-rapid/src/post.js
@@ -1,24 +1,22 @@
 const request = require('request-promise-native')
 
-post = (message, url) => new Promise((resolve, reject) => {
-  const req = request({
-    url: url,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(message),
-    resolveWithFullResponse: true
-  })
-
-  req
-    .then((response) => {
+post = (message, url) =>
+  new Promise((resolve, reject) => {
+    const req = request({
+      url: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+      resolveWithFullResponse: true,
     })
-    .catch((err) => {
+
+    req.then(response => {}).catch(err => {
       resolve()
-      var body = errormessage(err)
+      const body = errormessage(err)
       post(body, message.response_url)
     })
-})
+  })
 
 module.exports = post
