9fc05478d67ebab28e356ca6b650ce35bc8e81ba
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-11-06 23:18:47 -0800

    Moved events table into it's own component and adding sorting

--- create-event/src/components/Events.jsx
+++ create-event/src/components/Events.jsx
@@ -1,94 +1,37 @@
 // @flow
 
 import React, { Component } from 'react'
+import EventsTable from '../components/EventsTable'
+import columnData from '../constants/ColumnData'
 
-import Table, {
-  TableHead,
-  TableBody,
-  TableFooter,
-  TablePagination,
-  TableRow,
-  TableCell,
-} from 'material-ui/Table'
 import { CircularProgress } from 'material-ui/Progress'
 
-const data = [
-  { name: 'Name', attribute: 'title' },
-  { name: 'Location', attribute: 'venue_name' },
-  { name: 'Capacity', attribute: 'venue_capacity' },
-  { name: 'Status', attribute: 'status' },
-  { name: 'Date', attribute: 'start_date' },
-]
-
 class Events extends Component {
-  state = {
-    selected: [],
-    rowsPerPage: 5,
-    page: 0,
-  }
-
   componentWillMount() {
     this.props.fetchEvents()
   }
-
-  handleClick = (ev, id) => {
+  handleClick = id => ev => {
     ev.preventDefault()
     this.props.history.push(`/event/${id}`)
   }
-
-  handleChangePage = (ev, page) => {
-    this.setState({ page })
-  }
-
-  handleChangeRowsPerPage = ev => {
-    this.setState({ rowsPerPage: ev.target.value })
-  }
-
   render() {
-    const { page, rowsPerPage } = this.state
     const { events } = this.props
+
+    const loading = !events.length
     const noEvents = events.length <= 0
-    if (noEvents) {
+
+    if (loading) {
       return <CircularProgress />
+    } else if (noEvents) {
+      return <h3>You have no events.</h3>
     }
+
     return (
-      <div>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {data.map((n, i) => <TableCell key={i}>{n.name}</TableCell>)}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {events
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map(event => (
-                <TableRow
-                  key={event.id}
-                  hover
-                  onClick={ev => this.handleClick(ev, event.id)}
-                >
-                  {data.map((n, i) => (
-                    <TableCell key={i}>
-                      {event.attributes[n.attribute]}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                count={events.length}
-                page={page}
-                rowsPerPage={rowsPerPage}
-                onChangePage={this.handleChangePage}
-                onChangeRowsPerPage={this.handleChangeRowsPerPage}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
+      <EventsTable
+        data={events}
+        columnData={columnData}
+        handleRowClick={this.handleClick}
+      />
     )
   }
 }
---
+++ create-event/src/components/EventsTable.jsx
@@ -0,0 +1,116 @@
+// @flow
+
+import React, { Component } from 'react'
+
+import Table, {
+  TableHead,
+  TableBody,
+  TableFooter,
+  TablePagination,
+  TableRow,
+  TableCell,
+  TableSortLabel,
+} from 'material-ui/Table'
+import Tooltip from 'material-ui/Tooltip'
+
+class EventsTable extends Component {
+  state = {
+    data: [],
+    order: 'asc',
+    orderBy: 'title',
+    selected: [],
+    rowsPerPage: 5,
+    page: 0,
+  }
+
+  componentWillMount() {
+    const { data } = this.props
+    this.setState({ data })
+  }
+
+  componentWillReceiveProps({ data }) {
+    this.setState({ data })
+  }
+
+  handleChangePage = (ev, page) => {
+    this.setState({ page })
+  }
+
+  handleChangeRowsPerPage = ev => {
+    this.setState({ rowsPerPage: ev.target.value })
+  }
+
+  handleRequestSort = orderBy => ev => {
+    let order = 'desc'
+
+    if (this.state.orderBy === orderBy && this.state.order === 'desc') {
+      order = 'asc'
+    }
+
+    const data =
+      order === 'desc'
+        ? this.state.data.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+        : this.state.data.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1))
+
+    this.setState({ data, order, orderBy })
+  }
+
+  render() {
+    const { data, order, orderBy, page, rowsPerPage } = this.state
+    const { columnData, handleRowClick } = this.props
+
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columnData.map((n, i) => (
+              <TableCell key={i}>
+                <Tooltip
+                  title={
+                    orderBy !== n.attribute
+                      ? 'asc'
+                      : order === 'asc' ? 'desc' : 'asc'
+                  }
+                  enterDelay={300}
+                  placement="bottom-start"
+                >
+                  <TableSortLabel
+                    active={orderBy === n.attribute}
+                    direction={order}
+                    onClick={this.handleRequestSort(n.attribute)}
+                  >
+                    {n.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map(obj => (
+              <TableRow key={obj.id} hover onClick={handleRowClick(obj.id)}>
+                {columnData.map((n, i) => (
+                  <TableCell key={i}>{obj.attributes[n.attribute]}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              count={data.length}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              onChangePage={this.handleChangePage}
+              onChangeRowsPerPage={this.handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    )
+  }
+}
+
+export default EventsTable
