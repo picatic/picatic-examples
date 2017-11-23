fe5203995e740062978b46dc829b785fa8baa98a
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-11-23 13:18:53 -0800

    Cleaned up components and moved imports from public into source

--- react-checkout/src/index.js
+++ react-checkout/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/:id" component={App} />
-    </div>
-  </Router>,
+  <App />,
   document.getElementById('root')
 )
