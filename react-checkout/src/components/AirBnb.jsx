f74164ee9f915a85d67901610fd7f615161533a8
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2018-02-28 17:16:57 -0800

    Added default props to airbnb component

--- react-checkout/src/components/AirBnb.jsx
+++ react-checkout/src/components/AirBnb.jsx
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import { apiFetch } from '../utils/apiUtils'
 
 class AirBnb extends Component {
+  static defaultProps = {
+    eventId: 'event15198432929104',
+    airbnbApi: 'd306zoyjsyarp7ifhu67rjxn52tv0t20',
+    airbnbOauth: '9hpyczc6vjwr4cofnvdbokysn',
+    user_id: '175906773',
+    googleMapapikey: 'AIzaSyAUuwkvQXJdqclRNcchQTpQFJAMlGpxGO4',
+  }
   state = {
     airbnbEventid: 40069,
-    error: null
+    error: null,
   }
 
   componentWillMount() {
     const { eventId, eventObj } = this.props
     if (eventObj) {
       this.geoCode(eventObj)
-    }
-    else {
+    } else {
       this.getEvent(eventId)
     }
   }
@@ -23,7 +29,7 @@ class AirBnb extends Component {
 
     if (json) {
       const eventObj = json.data
-      console.log(eventObj.attributes.title)
+      console.log('title: ', eventObj.attributes.title)
       this.geoCode(eventObj)
     } else if (error) {
       this.setState({ error })
@@ -38,39 +44,52 @@ class AirBnb extends Component {
     if (json) {
       const lat = json.results[0].geometry.location.lat
       const lng = json.results[0].geometry.location.lng
-      this.createAirbnbEvent(eventObj, airbnbApi, airbnbOauth, user_id, lat, lng)
+      this.createAirbnbEvent(
+        eventObj,
+        airbnbApi,
+        airbnbOauth,
+        user_id,
+        lat,
+        lng,
+      )
     } else if (error) {
       this.setState({ error })
     }
   }
 
-
-  createAirbnbEvent = async (eventObj, airbnbApi, airbnbOauth, user_id, lat, lng) => {
+  createAirbnbEvent = async (
+    eventObj,
+    airbnbApi,
+    airbnbOauth,
+    user_id,
+    lat,
+    lng,
+  ) => {
     const eventData = eventObj.attributes
     const userId = Number(user_id)
     const url = `https://api.airbnb.com/v2/congregations?key=` + airbnbApi
     const body = JSON.stringify({
       name: eventData.title,
       address_1: eventData.venue_name,
-      address_2: "",
+      address_2: '',
       location: eventData.venue_street,
       check_in_at: eventData.start_date,
       check_out_at: eventData.end_date,
       guests: 1,
       lat: lat,
       lng: lng,
-      logo_url:"",
-      url:"https://www.picatic.com/"+eventObj.id,
-      user_id: userId
+      logo_url: '',
+      url: 'https://www.picatic.com/' + eventObj.id,
+      user_id: userId,
     })
-    console.log(body,lat,lng)
+    console.log(body, lat, lng)
     const { json, error } = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'X-Airbnb-OAuth-Token': airbnbOauth,
       },
-      body: body
+      body: body,
     })
       .then(res => res.json())
       .then(json => ({ json }))
@@ -88,11 +107,9 @@ class AirBnb extends Component {
   render() {
     const { airbnbEventid } = this.state
     const links = {
-      src: "https://events.withairbnb.com/index.html?eventid=" + airbnbEventid
+      src: 'https://events.withairbnb.com/index.html?eventid=' + airbnbEventid,
     }
-    return (
-      <iframe src={links.src} width="100%" height="100%" scrolling="no"></iframe>
-    )
+    return <iframe src={links.src} width="100%" height="100%" scrolling="no" />
   }
 }
 
